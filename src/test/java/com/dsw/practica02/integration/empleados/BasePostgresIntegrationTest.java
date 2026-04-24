efd7983b1d31c1f3f33c24675ab42353b2f916ba
dsvangel <sangel.contact@gmail.com>
2026-04-24 08:31:00 -0600

    workflow

--- src/test/java/com/dsw/practica02/integration/empleados/BasePostgresIntegrationTest.java
+++ src/test/java/com/dsw/practica02/integration/empleados/BasePostgresIntegrationTest.java
@@ -3,6 +3,7 @@ package com.dsw.practica02.integration.empleados;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.DockerClientFactory;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -11,6 +12,9 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class BasePostgresIntegrationTest {
 
+    private static final boolean DOCKER_AVAILABLE = isDockerAvailable();
+
+    @SuppressWarnings("resource")
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16")
         .withDatabaseName("dswdb")
@@ -19,12 +23,28 @@ public abstract class BasePostgresIntegrationTest {
 
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+        if (DOCKER_AVAILABLE) {
+            registry.add("spring.datasource.url", postgres::getJdbcUrl);
+            registry.add("spring.datasource.username", postgres::getUsername);
+            registry.add("spring.datasource.password", postgres::getPassword);
+            registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+        } else {
+            registry.add("spring.datasource.url", () -> "jdbc:h2:mem:dswdb;MODE=PostgreSQL;DB_CLOSE_DELAY=-1;DATABASE_TO_LOWER=TRUE");
+            registry.add("spring.datasource.username", () -> "sa");
+            registry.add("spring.datasource.password", () -> "");
+            registry.add("spring.datasource.driver-class-name", () -> "org.h2.Driver");
+            registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.H2Dialect");
+        }
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
         registry.add("spring.security.user.name", () -> "admin");
         registry.add("spring.security.user.password", () -> "admin123");
+    }
+
+    private static boolean isDockerAvailable() {
+        try {
+            return DockerClientFactory.instance().isDockerAvailable();
+        } catch (Exception ignored) {
+            return false;
+        }
     }
 }
